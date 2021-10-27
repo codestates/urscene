b49ac737399d1ecbf35d5f6671847b9b709857c6
songha <songha@songhaui-MacBookPro.local>
2021-10-27 17:41:10 +0900

    Feat : Update Footer css

--- client/src/pages/Gallery.js
+++ client/src/pages/Gallery.js
@@ -2,11 +2,8 @@ import React, { useState } from "react";
 import MainNav from "../components/MainNav";
 import GalleryContent from "../components/GalleryContent";
 import GalleryDeleteModal from "../components/GalleryDeleteModal";
-import DeleteModal from "../components/DeleteModal";
-import { Link } from "react-router-dom";
 import MainFooter from "../components/MainFooter";
 import TopButton from "../components/TopButton";
-
 
 function Gallery() {
   const [likeModal, setlikeModal] = useState(false); // 좋아요 버튼 false가 안누른상태
@@ -27,27 +24,52 @@ function Gallery() {
             <div className="gallery-group1">
               {editModal ? (
                 <div>
-                  <textarea className="gallery-titleinput">로맨스만 모았다.</textarea>
-                  <div className="gallery-btn" onClick={() => setEditModal(!editModal)}>
+                  <textarea className="gallery-titleinput">
+                    로맨스만 모았다.
+                  </textarea>
+                  <div
+                    className="gallery-btn"
+                    onClick={() => setEditModal(!editModal)}
+                  >
                     완료
-
                   </div>
                 </div>
               ) : (
                 <div>
                   <div className="gallery-title">로맨스만 모았다.</div>
-                  <div className="gallery-edit" onClick={() => setEditModal(!editModal)}></div>
-                  <div className="gallery-delete" onClick={() => setDeleteModal(!deleteModal)}></div>
+                  <div
+                    className="gallery-edit"
+                    onClick={() => setEditModal(!editModal)}
+                  ></div>
+                  <div
+                    className="gallery-delete"
+                    onClick={() => setDeleteModal(!deleteModal)}
+                  ></div>
                 </div>
               )}
             </div>
             <div className="gallery-group2">
               {editModal ? (
-                <textarea className="gallery-descinput">로맨스만 모았다.</textarea>
+                <textarea className="gallery-descinput">
+                  로맨스만 모았다.
+                </textarea>
               ) : (
                 <div>
-                  <div className="gallery-desc">로맨스라는 로맨스라는 로맨스 사랑이 넘치는 로맨스장면들만 가득있는 곳 </div>
-                  {likeModal ? <div className="gallery-like2" onClick={() => setlikeModal(!likeModal)}></div> : <div className="gallery-like1" onClick={() => setlikeModal(!likeModal)}></div>}
+                  <div className="gallery-desc">
+                    로맨스라는 로맨스라는 로맨스 사랑이 넘치는 로맨스장면들만
+                    가득있는 곳{" "}
+                  </div>
+                  {likeModal ? (
+                    <div
+                      className="gallery-like2"
+                      onClick={() => setlikeModal(!likeModal)}
+                    ></div>
+                  ) : (
+                    <div
+                      className="gallery-like1"
+                      onClick={() => setlikeModal(!likeModal)}
+                    ></div>
+                  )}
                 </div>
               )}
             </div>
@@ -67,7 +89,9 @@ function Gallery() {
           <TopButton></TopButton>
         </div>
       </div>
-      {deleteModal ? <GalleryDeleteModal handleDeleteModal={handleDeleteModal} /> : null}
+      {deleteModal ? (
+        <GalleryDeleteModal handleDeleteModal={handleDeleteModal} />
+      ) : null}
     </div>
   );
 }
