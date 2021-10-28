12bdfdd02f04096aac8653e580873bf11012ef14
kangseokho <kjssaa3@gmail.com>
2021-10-28 13:48:19 +0900

    feat:Add searched controller

--- server/routes/searchedgallery.route.js
+++ server/routes/searchedgallery.route.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const searchcontroller = require("../controllers/search.controller")
+const searchcontroller = require("../controllers/searchedgallery.controller")
 const router = express.Router()
 
 router.get("/", searchcontroller.get)
