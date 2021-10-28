9eb3d6ed3a58345c36bab81a630f553ad99f25d0
songha <songha@songhaui-MacBookPro.local>
2021-10-28 11:47:37 +0900

    feat : Add main page codes and fix css

---
+++ client/src/components/dummy/mainGenre.js
@@ -0,0 +1,125 @@
+const mainGenre = {
+  single: [
+    {
+      id: 1,
+      title: "오징어게임",
+      genre: "로맨스",
+      content: "456억은 내꺼",
+      image: "client/src/img/UserImage-Jake.png",
+      description_id: 1,
+      createdAt: "2021-10-26T04:47:35.000Z",
+      updatedAt: "2021-10-26T04:47:30.000Z",
+    },
+    {
+      id: 2,
+      title: "오징어게임",
+      genre: "로맨스",
+      content: "456억은 내꺼",
+      image: "./dummy/img/romance/2",
+      description_id: 2,
+      createdAt: "2021-10-26T04:47:35.000Z",
+      updatedAt: "2021-10-26T04:47:30.000Z",
+    },
+    {
+      id: 3,
+      title: "오징어게임",
+      genre: "로맨스",
+      content: "456억은 내꺼",
+      image:
+        "/Users/songha/Documents/urscene/client/src/components/dummy/img/romance/3.jpg",
+      description_id: 3,
+      createdAt: "2021-10-26T04:47:35.000Z",
+      updatedAt: "2021-10-26T04:47:30.000Z",
+    },
+    {
+      id: 4,
+      title: "오징어게임",
+      genre: "로맨스",
+      content: "456억은 내꺼",
+      image:
+        "/Users/songha/Documents/urscene/client/src/components/dummy/img/romance/3.jpg",
+      description_id: 3,
+      createdAt: "2021-10-26T04:47:35.000Z",
+      updatedAt: "2021-10-26T04:47:30.000Z",
+    },
+    {
+      id: 5,
+      title: "오징어게임",
+      genre: "로맨스",
+      content: "456억은 내꺼",
+      image:
+        "/Users/songha/Documents/urscene/client/src/components/dummy/img/romance/3.jpg",
+      description_id: 3,
+      createdAt: "2021-10-26T04:47:35.000Z",
+      updatedAt: "2021-10-26T04:47:30.000Z",
+    },
+    {
+      id: 6,
+      title: "어바웃타임",
+      genre: "로맨스",
+      content: "456억은 내꺼",
+      image:
+        "/Users/songha/Documents/urscene/client/src/components/dummy/img/romance/3.jpg",
+      description_id: 3,
+      createdAt: "2021-10-26T04:47:35.000Z",
+      updatedAt: "2021-10-26T04:47:30.000Z",
+    },
+    {
+      id: 7,
+      title: "극한직업",
+      genre: "코미디",
+      content: "456억은 내꺼",
+      image:
+        "/Users/songha/Documents/urscene/client/src/components/dummy/img/romance/3.jpg",
+      description_id: 3,
+      createdAt: "2021-10-26T04:47:35.000Z",
+      updatedAt: "2021-10-26T04:47:30.000Z",
+    },
+    {
+      id: 8,
+      title: "오징어게임",
+      genre: "SF/판타지",
+      content: "456억은 내꺼",
+      image:
+        "/Users/songha/Documents/urscene/client/src/components/dummy/img/romance/3.jpg",
+      description_id: 3,
+      createdAt: "2021-10-26T04:47:35.000Z",
+      updatedAt: "2021-10-26T04:47:30.000Z",
+    },
+    {
+      id: 9,
+      title: "미션임파서블",
+      genre: "액션",
+      content: "456억은 내꺼",
+      image:
+        "/Users/songha/Documents/urscene/client/src/components/dummy/img/romance/3.jpg",
+      description_id: 3,
+      createdAt: "2021-10-26T04:47:35.000Z",
+      updatedAt: "2021-10-26T04:47:30.000Z",
+    },
+    {
+      id: 10,
+      title: "겟아웃",
+      genre: "미스터리/스릴러",
+      content: "456억은 내꺼",
+      image:
+        "/Users/songha/Documents/urscene/client/src/components/dummy/img/romance/3.jpg",
+      description_id: 3,
+      createdAt: "2021-10-26T04:47:35.000Z",
+      updatedAt: "2021-10-26T04:47:30.000Z",
+    },
+    {
+      id: 11,
+      title: "전쟁영화",
+      genre: "전쟁",
+      content: "456억은 내꺼",
+      image:
+        "/Users/songha/Documents/urscene/client/src/components/dummy/img/romance/3.jpg",
+      description_id: 3,
+      createdAt: "2021-10-26T04:47:35.000Z",
+      updatedAt: "2021-10-26T04:47:30.000Z",
+    },
+  ],
+};
+
+export default mainGenre;
