21d821df9bb08a1785c7de849b9d33f6a6827e0f
usreon <sonyeonju7@gmail.com>
2021-10-29 22:23:20 +0900

    feat: Add lib foler in server

--- server/lib/jwt.js
+++ server/lib/jwt.js
@@ -7,7 +7,7 @@ module.exports = {
 		} catch (err) {
 			/** * 다음과 같은 형태로 특정 에러에 대해서 핸들링해줄 수 있다.
 			 * if (e.name === 'TokenExpiredError') { return null } * */
-			// return null 유효기간이 만료된 코드에 대해서 null을 리턴한다.
+			// return null 유효기간이 만료된 코드에 대해서 null을 리턴한다. 추가 작성
 		}
 	},
 }
--- server/lib/regex.js
+++ server/lib/regex.js
@@ -1,7 +1,7 @@
 const emailValidation = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 const passwordValidation = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$/
 
-const emailRegex = (email) => emailValidation.test(email) // 통과하면 true return
+const emailRegex = (email) => emailValidation.test(email) // 통과하면 true
 const passwordRegex = (password) => passwordValidation.test(password)
 const validatorForEmail = (email) => {
 	if (!emailRegex(email)) {
