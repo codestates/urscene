c77865c4b6dbe607e7b2561403e9d435b3ff99b7
kangseokho <kjssaa3@gmail.com>
2021-10-29 15:05:50 +0900

    revise :Docs main controller

--- server/controllers/main.controller.js
+++ server/controllers/main.controller.js
@@ -1,12 +1,23 @@
-const { Singlepost, Like, User, Gallerypost, sequelize } = require("../models")
+const { Singlepost, Singlepost_gallerypost, Like, User, Gallerypost, sequelize } = require("../models")
 const { Op } = require("sequelize")
 
 module.exports = {
 	genre: async (req, res) => {
+		const { genre, page, limit } = req.query
+		// console.log(genre, page, limit)
+		let offset = 0
+
+		if (page > 1) {
+			offset = 1 * (page - 1)
+		}
+
 		let single = await Singlepost.findAll({
 			order: [["createdAt", "DESC"]],
-			where: { genre: req.params.genre },
+			where: { genre: genre },
+			offset: offset,
+			limit: Number(limit),
 		})
+
 		// console.log(single)
 		if (!single) {
 			res.status(404).json({ message: "data-not-found" })
@@ -20,20 +31,70 @@ module.exports = {
 	},
 
 	rank: async (req, res) => {
+		// const data = await Singlepost_gallerypost.findOne({
+		// 	include: [],
+		// 	where: { Gallerypost_id: 1 },
+		// })
+		// console.log(data)
+
 		const data = await sequelize.query(
 			"SELECT Galleryposts.id, count(Likes.id) AS count FROM Galleryposts INNER JOIN Likes ON Galleryposts.id = Likes.gallerypost_id Group by Galleryposts.id ORDER BY count desc LIMIT 9"
 		)
 		const likelist = data[0]
 		// console.log(likelist)
 		let array = []
-		for (const el of likelist) {
-			const data = await Gallerypost.findOne({
-				where: { id: el.id },
+		let image = []
+		for (const element of likelist) {
+			const singleimage = await Singlepost_gallerypost.findAll({
+				include: [
+					{
+						model: Singlepost,
+						attributes: ["image"],
+					},
+				],
+				where: { Gallerypost_id: element.id },
 			})
-			delete data.dataValues.user_id
-			array.push(data.dataValues)
+			const data = singleimage.map((el) => el.dataValues.Singlepost.dataValues)
+			image.push(data)
+		}
+		// console.log(image)
+
+		for (const element of likelist) {
+			const gallery = await Singlepost_gallerypost.findOne({
+				include: [
+					{
+						model: Gallerypost,
+						attributes: ["id", "title", "content"],
+					},
+				],
+				where: { Gallerypost_id: element.id },
+			})
+			const data = gallery.dataValues.Gallerypost.dataValues
+			array.push(data)
 		}
 		// console.log(array)
+
+		array.map((el, index) => {
+			el.image = image[index]
+		})
+
+		// gallery의 요소하나에 image의 요소하나를 넣는다
+
+		// for (const el of likelist) {
+		// 	const data = await Gallerypost.findOne({
+		// 		include: [
+		// 			{
+		// 				model: User,
+		// 				attributes: ["nickname"],
+		// 			},
+		// 		],
+		// 		where: { id: el.id },
+		// 	})
+		// 	delete data.dataValues.user_id
+		// 	array.push(data.dataValues)
+		// }
+		// console.log(array)
+
 		// const map = await likelist.forEach(async (el) => {
 		// 	const data = await Gallerypost.findOne({
 		// 		where: { id: el.id },
