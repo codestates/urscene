5a260dfc38dcddd9f2dfc6756eadf364e711a3bf
usreon <sonyeonju7@gmail.com>
2021-10-29 22:22:11 +0900

    move: Make folder in controllers

--- server/controllers/sign/signout.js
+++ server/controllers/sign/signout.js
@@ -2,5 +2,10 @@ const db = require("../../db")
 require("dotenv").config()
 
 module.exports = {
-	signOut: async (req, res) => {},
+	signOut: async (req, res) => {
+		try {
+		} catch (err) {
+			return res.status(500).json({ message: "sever-error" })
+		}
+	},
 }
