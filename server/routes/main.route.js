d77c2e70ae43d7017ba92072a2669f7a4f4a3592
usreon <sonyeonju7@gmail.com>
2021-10-29 22:16:33 +0900

    remove: Delete this folder

--- server/routes/main.route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const maincontroller = require("../controllers/main.controller")
-const router = express.Router()
-
-router.get("/single/:genre", maincontroller.genre)
-router.get("/", maincontroller.rank)
-
-module.exports = router
--- server/routes/searchedgallery.route.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-const searchcontroller = require("../controllers/searchedgallery.controller")
-const router = express.Router()
-
-router.get("/", searchcontroller.get)
-
-module.exports = router
