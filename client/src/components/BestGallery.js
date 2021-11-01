e79934adf6515d66e5ef6f066301810fe7446c26
songha <songha@songhaui-MacBookPro.local>
2021-11-01 20:21:39 +0900

    merge

--- client/src/components/BestGallery.js
+++ client/src/components/BestGallery.js
@@ -1,17 +1,17 @@
 import React from "react";
 
 function BestGallery({ gallery }) {
-  const rendering = [];
-  for (let i = 0; i < 4; i++) {
-    rendering.push(gallery.image[i]);
-  }
+  // const rendering = [];
+  // for (let i = 0; i < 4; i++) {
+  //   rendering.push(gallery.image[i]);
+  // }
 
   return (
     <div>
       <div className="BG-container">
         <div className="BG-title">{gallery.title}</div>
         <div className="BG-img-wrap">
-          {rendering.map((ele, idx) => {
+          {gallery.image.map((ele, idx) => {
             return (
               <div className="BG-img" key={idx}>
                 <img src={ele.image} alt="" />
