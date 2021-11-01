e7053cc04bb36894e45c7633d3e3ed7a9a592af6
songha <songha@songhaui-MacBookPro.local>
2021-11-01 16:04:16 +0900

    feat : Add post page and writecomment

--- server/controllers/singlepost/getSinglepost.js
+++ server/controllers/singlepost/getSinglepost.js
@@ -1,28 +1,28 @@
-const { Singlepost, Description, User } = require("../../models")
+const {Singlepost, Description, User} = require("../../models");
 
 module.exports = async (req, res) => {
-	const userinfo = getverify(req.cookies.jwt)
-	const { singlepostid } = req.params
-	let post = await Singlepost.findOne({
-		include: [
-			{
-				model: User,
-				attributes: ["nickname", "image"],
-			},
-			{
-				model: Description,
-				attributes: ["title", "title_eng", "genre", "director", "released"],
-			},
-		],
-		where: { id: singlepostid },
-	})
-	if (!post) {
-		//singlepostid 없으면 404
-		res.status(404).json({ message: "data-not-found" })
-	} else {
-		delete post.dataValues.user_id
-		res.status(200).json({ data: post.dataValues })
-	} // post.dataValues => id,user_id,title,genre,content,image(장면),description_id
-	// 영화정보에는 title,genre,director,released 나의장면 상세페이지의 영화장르는 현재 앱에서 지원하는 장르와 다른컨셉.
-	// 왜 다른컨셉?? 사용자의 취향존중 ok
-}
+  // const userinfo = getverify(req.cookies.jwt)
+  const {singlepostid} = req.params;
+  let post = await Singlepost.findOne({
+    include: [
+      {
+        model: User,
+        attributes: ["nickname", "image"],
+      },
+      {
+        model: Description,
+        attributes: ["title", "title_eng", "genre", "director", "released"],
+      },
+    ],
+    where: {id: singlepostid},
+  });
+  if (!post) {
+    //singlepostid 없으면 404
+    res.status(404).json({message: "data-not-found"});
+  } else {
+    delete post.dataValues.user_id;
+    res.status(200).json({data: post.dataValues});
+  } // post.dataValues => id,user_id,title,genre,content,image(장면),description_id
+  // 영화정보에는 title,genre,director,released 나의장면 상세페이지의 영화장르는 현재 앱에서 지원하는 장르와 다른컨셉.
+  // 왜 다른컨셉?? 사용자의 취향존중 ok
+};
