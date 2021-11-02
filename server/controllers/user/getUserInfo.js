767c51be067f8d3b9e2f55533a647709741284ae
usreon <sonyeonju7@gmail.com>
2021-11-02 19:52:05 +0900

    style: Remove unnecessary comment

--- server/controllers/user/getUserInfo.js
+++ server/controllers/user/getUserInfo.js
@@ -1,4 +1,3 @@
-const jwt = require("jsonwebtoken")
 const db = require("../../db")
 const { decrypt, isAuthorized } = require("../../lib/jwt")
 
@@ -6,18 +5,18 @@ require("dotenv").config()
 
 module.exports = async (req, res) => {
 	try {
-		const cookieUUID = req.cookies.uuid // 암호화 안됐음
+		const cookieUUID = req.cookies.uuid
 		const userToken = isAuthorized(req)
 		if (!userToken) {
 			return res.status(400).json({ message: "not-authorized" })
 		}
 
-		const { uuid } = userToken
+		const { id, uuid } = userToken
 		const decryptedUUID = await decrypt(uuid, process.env.ENCRYPTION_KEY)
 
 		if (decryptedUUID === cookieUUID) {
-			const user = await db.getUserByEmail(userToken.email)
-			const { id, email, nickname, image } = user.dataValues
+			const user = await db.getUserById(id)
+			const { email, nickname, image } = user.dataValues
 			return res.status(200).json({ id, email, nickname, image })
 		}
 		return res.status(400).json({ message: "invalid-token" })
