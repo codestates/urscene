93f13ebba9f7e129100e0f298fe6d13eb198f0ed
usreon <sonyeonju7@gmail.com>
2021-11-02 19:53:16 +0900

    feat: Add withdraw user function

---
+++ server/controllers/user/withdrawUser.js
@@ -0,0 +1,30 @@
+const { db } = require("../../db")
+const { isAuthorized } = require("../../lib/jwt")
+
+module.exports = async (req, res) => {
+	try {
+		const userToken = isAuthorized(req)
+		if (!userToken) {
+			return res.status(400).json({ message: "not-authorized" })
+		}
+
+		const { id, uuid } = userToken
+		const decryptedUUID = await decrypt(uuid, process.env.ENCRYPTION_KEY)
+		const cookieUUID = req.cookies.uuid
+
+		if (decryptedUUID === cookieUUID) {
+			await db.deleteUser(id)
+			res
+				.clearCookie("token", {
+					httpOnly: true,
+					sameSite: "none",
+					secure: true,
+				})
+				.status(205)
+				.json({ message: "user-deleted-successfully" })
+		}
+		res.status(401).json({ message: "invalid-token" })
+	} catch (err) {
+		res.status(500).json({ message: "server-error" })
+	}
+}
