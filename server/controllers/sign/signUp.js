3468c709ef117649051114baa3b154186ccab97b
usreon <sonyeonju7@gmail.com>
2021-11-07 20:11:15 +0900

    feat: Add bcrypt for password

--- server/controllers/sign/signUp.js
+++ server/controllers/sign/signUp.js
@@ -1,17 +1,15 @@
-const { validator } = require("../../lib/regex")
+const bcrypt = require("bcrypt")
 const db = require("../../db")
-require("dotenv").config()
+const { validator } = require("../../lib/regex")
 
 module.exports = async (req, res) => {
 	try {
 		const { email, nickname, password, image } = req.body
-
 		const isInvalid = validator(email, password, nickname)
 		if (isInvalid) {
 			return res.status(isInvalid.code).json({ message: isInvalid.message })
 		}
-		// 만약 이미지가 없는 경우 디폴트 이미지 정해줘서 보내줘야 할까? (프론트분들과 상의)
-		// 비밀번호 암호화 해서 넣기
+
 		const userByEmail = await db.getUserByEmail(email)
 		if (userByEmail) {
 			return res.status(409).send({ email, message: "email-aready-exists" })
@@ -22,7 +20,9 @@ module.exports = async (req, res) => {
 			return res.status(409).send({ nickname, message: "name-aready-exists" })
 		}
 
-		await db.addUser({ email, nickname, password, image })
+		const salt = await bcrypt.genSalt(10)
+		const hashPassword = await bcrypt.hash(password, salt)
+		await db.addUser({ email, nickname, password: hashPassword, image })
 		res.status(201).json({ nickname, message: "user-created" })
 	} catch (err) {
 		res.status(500).json({ message: "server-error" })
