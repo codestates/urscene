ca42b4c8e105f2111422621493e7642d775c6179
usreon <sonyeonju7@gmail.com>
2021-11-11 16:14:59 +0900

    fix: Change google redirect uri

--- server/lib/regex.js
+++ server/lib/regex.js
@@ -1,7 +1,7 @@
 const emailValidation = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-const passwordValidation = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$/
+const passwordValidation = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/
 
-const emailRegex = (email) => emailValidation.test(email) // 통과하면 true return
+const emailRegex = (email) => emailValidation.test(email)
 const passwordRegex = (password) => passwordValidation.test(password)
 const validator = (email, password, nickname) => {
 	if (!email || !emailRegex(email)) {
